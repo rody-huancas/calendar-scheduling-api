8f683b2b162e882846289b05d64a3386044d7add
Rody Huancas <rodyhuancas.04@gmail.com>
2025-04-23 20:53:30 -0500

    Feat: add functionality to retrieve availability for public events with validation and service integration

--- src/routes/avalability.route.ts
+++ src/routes/avalability.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { passportAuthenticateJwt } from "../config/passport.config";
-import { getUserAvailabilityController, updateAvailabilityController } from "../controllers/availability.controller";
+import { getAvailabilityForPublicEventController, getUserAvailabilityController, updateAvailabilityController } from "../controllers/availability.controller";
 
 const availabilityRoutes = Router();
 
 availabilityRoutes.get("/me", passportAuthenticateJwt, getUserAvailabilityController);
+
+availabilityRoutes.get("/public/:eventId", getAvailabilityForPublicEventController);
+  
 availabilityRoutes.put("/update", passportAuthenticateJwt, updateAvailabilityController);
 
 export default availabilityRoutes;
